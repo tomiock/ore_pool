e66bfaa660b10d3742ac7a764ea91282a6055e71
tomiock <ockier1@gmail.com>
2024-08-09 20:36:54 +0200

    my shell, i need it

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  # Packages that you want to include in your shell environment
+  buildInputs = [
+    pkgs.go           # The Go compiler and tools
+    pkgs.git          # Git, in case you need version control
+    pkgs.air
+  ];
+
+  # Set the GO111MODULE environment variable (optional)
+  GO111MODULE = "on";
+
+  # Set additional environment variables if necessary
+  shellHook = ''
+    export GOPATH=$HOME/go
+    echo "HTTP Server and SH Script"
+  '';
+}
+
